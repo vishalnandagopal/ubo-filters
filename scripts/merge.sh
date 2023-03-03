8fb56f90a266d76e12f2449091b61957c27bf50e
Vishal N <61023404+vishalnandagopal@users.noreply.github.com>
2023-03-03 14:02:38 +0530

    merge with sh instead of py

---
+++ scripts/merge.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+boilerplate=$"[Adblock Plus 2.0]
+! Title: Vishal's personal uBO filter list
+! Last modified: `date`
+! Expires: 7 days (update frequency)
+!
+! Please report only issues or problems specifically with this filter list via GitHub issues or via e-mail (ubo-filters@vishalnandagopal.com). If you see ads not being blocked, report it on EasyList forums - https://forums.lanik.us/ .
+!
+! Homepage: https://github.com/vishalnandagopal/ubo-filters
+! Licence: https://github.com/vishalnandagopal/ubo-filters/blob/main/LICENSE\
+! GitHub issues: https://github.com/vishalnandagopal/ubo-filters/issues
+! GitHub pull requests: https://github.com/vishalnandagopal/ubo-filters/pulls
+! ----------------------------- FILTER LIST BEGINS -----------------------------
+"
+
+lastline=$"
+
+----------------------------- FILTER LIST ENDS -----------------------------"
+
+if ls | grep -Fq "local-filters.txt"; then
+    echo "$boilerplate" > my-filters.txt
+    cat local-filters.txt >> my-filters.txt
+    echo "$lastline" >> my-filters.txt
+else
+    echo 'local-filters.txt not found'
+fi
